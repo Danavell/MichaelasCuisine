6084aa038eb2eb44d414c2ab0fa82e9f5f571309
Danavell <jamesdanavell@gmail.com>
2020-09-07 16:22:25 +0100

    fixes scrolling issue in product

--- src/Components/Product/product.js
+++ src/Components/Product/product.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { isMobile } from 'react-device-detect';
 import { useSelector } from 'react-redux'
 
@@ -8,6 +8,10 @@ import Hero from '../ProductHero/productHero'
 import styles from './product.module.css'
 
 const Product = (props) => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
 
     let id = parseInt(props.match.params.productId)
 
